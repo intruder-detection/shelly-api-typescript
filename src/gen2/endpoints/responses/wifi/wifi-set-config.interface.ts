b5763ac4ad92cf2166d25c948790b66c9d68f846
Rui Alves Personal <alvesrui951@gmail.com>
2024-06-14 12:44:38 +0100

    Added ts-essentials

--- src/gen2/endpoints/responses/wifi/wifi-set-config.interface.ts
+++ src/gen2/endpoints/responses/wifi/wifi-set-config.interface.ts
@@ -1,7 +1,7 @@
-import { DeepPartial } from '@utils/extra.types';
 import { BaseResponse } from '@gen2/generic.types';
 import { WifiAccessPointWithPass, WifiRoaming, WifiStationWithPass } from '@gen2/endpoints/responses/wifi/common-interfaces.types';
 import { RestartRequired } from '@gen2/endpoints/responses/common/restart-required.types';
+import { DeepPartial } from 'ts-essentials';
 
 export interface WifiSetConfigRequest {
   config: DeepPartial<WifiConfigForSetConfiguration>;
